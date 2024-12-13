cdf0c136f547d2944bfacf81cfeba5a15b31455e
Gian Daria <gian.daria@outlook.com>
2024-12-13 19:17:34 +0800

    End of section 10

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -8,12 +9,12 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> 
+        public class Query : IRequest<Result<List<Activity>>> 
         {
 
         }
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
             private readonly DataContext _context;
             // private readonly ILogger<List> _logger;
@@ -26,7 +27,7 @@ namespace Application.Activities
             }
 
             // public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 // try
                 // {
@@ -42,7 +43,9 @@ namespace Application.Activities
                 //     _logger.LogInformation("Task was cancelled");
                 // }
 
-                return await _context.Activities.ToListAsync();
+                var result = await _context.Activities.ToListAsync();
+
+                return Result<List<Activity>>.Success(result);
             }
         }
     }
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import App from './app/layout/App'
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/ReactToastify.min.css'
 import './app/layout/styles.css';
 import { store, StoreContext } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
@@ -11,7 +11,6 @@ import { router } from './app/router/Routes';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StoreContext.Provider value={store}>
-      {/* <App /> */}
       <RouterProvider router={router} />
     </StoreContext.Provider>
   </StrictMode>
